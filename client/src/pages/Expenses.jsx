16a0b81a44208ddf5e3f92b8850bf6911f998722
Henry Wu <hhw67865@gmail.com>
2024-03-13 23:53:00 -0400

    fix(dls): fixed time problem with daylight savings

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -18,12 +18,16 @@ const Expenses = ({ getExpenses, getCategories }) => {
   let dayOfWeek = startOfWeek.getDay();
   dayOfWeek = (dayOfWeek === 0 ? 7 : dayOfWeek) - 1; // Adjust for Monday start
   startOfWeek.setDate(startOfWeek.getDate() - dayOfWeek);
+  startOfWeek.setHours(0, 0, 0, 0); // Set the time to the start of the day to avoid DST issues
+  console.log(startOfWeek)
   const endOfWeek = new Date(startOfWeek);
   endOfWeek.setDate(endOfWeek.getDate() + 6);
-
+  endOfWeek.setHours(23, 59, 59, 999); // Set the time to the end of the day to avoid DST issues
+  
   const weeklyExpenses = getExpenses.data.filter((expense) => {
     const dateParts = expense.date.split("-");
     const date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+    date.setHours(12, 0, 0, 0); // Set the time to midday to avoid DST issues
     return date >= startOfWeek && date <= endOfWeek;
   });
 
