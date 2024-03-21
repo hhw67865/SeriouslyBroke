d163c215772d832982f73a89150a7d599b602fd6
Henry Wu <hhw67865@gmail.com>
2024-03-21 10:50:06 -0400

    style(categories): added scrolling

--- client/src/features/category/components/CategorySelectionContainer.jsx
+++ client/src/features/category/components/CategorySelectionContainer.jsx
@@ -16,7 +16,7 @@ const CategorySelectionContainer = ({
       <div className="mb-4">
         <h1 className="text-xl font-bold">Categories</h1>
       </div>
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4 overflow-auto max-h-[32rem]">
         {getCategories.data.map((category) => (
           <CategorySelectionCard
             key={category.id}
